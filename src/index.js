9391453822280ff3c28150ee40b1f41f5774c284
Cameron Abel <cameronabel@gmail.com>
2023-01-17 19:10:27 -0800

    functional basic api call

--- src/index.js
+++ src/index.js
@@ -1,45 +1,35 @@
 import '../src/css/style.css'
+const { Configuration, OpenAIApi } = require("openai");
 
-//business logic
-function getAPIData(response) {
-  //generate prompts?
-  //
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+async function getAPIData() {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: localStorage.runningPrompt,
+    max_tokens:350,
+    temperature: 0,
+  });
+  return response;
 }
-//function determine an end of game???? How long it is? 
-  //how many calls we make in total that determines a length of the game?
-
-
-// function that initial prompt sender
-    //*prompt writer => game design area
-    // determines the theme
-    // defines the rules of the game, the limitations
-    //defines user input? (1,2,3) How many choices can the player make?
-
-
-//UI logic 
 
 function startGame(){
- let startBox = document.getElementById("startBox");
- startBox.classList.add("hidden");
- let gameBox = document.getElementById("gameBox");
- gameBox.classList.remove("hidden");
+  const theme = document.getElementById('themeInput').value || 'fantasy';
+  initializePrompt(theme);
+  let startBox = document.getElementById("startBox");
+  startBox.classList.add("hidden");
+  let gameBox = document.getElementById("gameBox");
+  gameBox.classList.remove("hidden");
+  getAPIData().then(function (resp) {console.log(resp)})
+}
+
+function initializePrompt(theme) {
+  localStorage.runningPrompt = `Greg is a choose-your-own-adventure generator in a ${theme} setting. He gives the user a scene description of between 100 and 200 words that contains one distinct feature or character. At the end of the scene description, he names and describes that feature in exactly 5 words, prefixed with the word "Feature>". Greg then offers three choices for the user to select to move onto the next scene. After 3 scenes, he concludes the adventure.`;
 }
 
 document.getElementById("startButton").addEventListener('click', startGame);
-
-//function that print responses
-function printElements() {
-  //print ai completions?
-  document.getElementById("aiInput").innerText = output;
-}
-
-function printError() {
-  
-}
-//function that display images
-
-
-
-
-
-
